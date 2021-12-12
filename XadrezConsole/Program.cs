8d5943063a4b257f544105269c61d0be108bb740
André Sernajoto <andre.sernajoto@gmail.com>
2021-12-12 15:28:11 -0300

    Criação da classe PosicaoXadrez

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -8,17 +8,10 @@ namespace XadrezConsole {
     class Program {
         static void Main(string[] args) {
 
-            try {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+            PosicaoXadrez posXadrez = new PosicaoXadrez('c', 7);
 
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                Tela.ImprimirTabuleiro(tab);
-            } catch (TabuleiroException e) {
-                Console.WriteLine("Erro no posicionamento: {0}", e.Message);
-            }
+            Console.WriteLine(posXadrez);
+            Console.WriteLine(posXadrez.ConverterPosicao());
         }
     }
 }
---
+++ XadrezConsole/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,25 @@
+﻿using XadrezConsole.Quadro;
+
+namespace XadrezConsole.Xadrez {
+    class PosicaoXadrez {
+        // declaração das propriedades da classe
+        public char Coluna { get; set; }
+        public int Linha { get; set; }
+
+        // construtor que recebe uma letra para a coluna e um número para linha
+        public PosicaoXadrez(char coluna, int linha) {
+            Coluna = coluna;
+            Linha = linha;
+        }
+
+        // método que converte as posições da matriz para as do tabuleiro de xadrez
+        public Posicao ConverterPosicao() {
+            return new Posicao(8 - Linha, Coluna - 'a');
+        }
+
+        // override da impressão das propriedades
+        public override string ToString() {
+            return "" + Coluna + Linha;
+        }
+    }
+}
